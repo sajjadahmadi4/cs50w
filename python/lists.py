03b511cfe90bd0009e05e79e264aae9c7d2217af
Sajjad <sajjadahmadi497@gmail.com>
2022-03-18 11:31:36 -0700

    practiced basic python data structures

---
+++ python/lists.py
@@ -0,0 +1,10 @@
+# practicing python data structures -> lists
+
+# define a list of names
+names = ["Harry", "Ron", "Hermione", "Giny"]
+
+names.append("Draco")
+
+names.sort()
+
+print(names)
---
+++ python/sets.py
@@ -0,0 +1,19 @@
+# practicing sets in python
+
+# create an empty set
+s = set()
+
+# add elements to the set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+
+# remove an element from the set
+s.remove(2)
+
+# len function returns the size of a sequence
+print(f"The set has {len(s)} elements")
+
+# print the set
+print(s)
